844a5c4126dbe762bd60fcc55a6d58fc3b333fda
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2020-07-31 14:49:22 -0500

    change readme file

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -3,17 +3,18 @@ const Big = require('big.js');
 const Operate = (numberOne, numberTwo, operation) => {
   const nOne = new Big(numberOne);
   const nTwo = new Big(numberTwo);
+
   switch (operation) {
     case '-':
-      return nOne - nTwo;
+      return nOne.minus(nTwo);
     case '+':
-      return nOne + nTwo;
+      return nOne.plus(nTwo);
     case 'x':
-      return nOne * nTwo;
+      return nOne.times(nTwo);
     case '÷':
-      return nOne / nTwo;
+      return nOne.div(nTwo);
     case '%':
-      return nOne - nTwo;
+      return nOne.minus(nTwo);
     default:
       break;
   }
